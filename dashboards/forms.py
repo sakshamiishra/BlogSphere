eef18678d1f0ba3c71d6927ecf3a01a638e9b9fc
Saksham Mishra <sakshammishra0018@gmail.com>
2026-06-08 16:42:53 +0530

    category functioality except two function that will be added in nect commit

---
+++ dashboards/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from blogs.models import Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
